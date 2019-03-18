4fc3e45686416430caf6352537e924912ffd1f19
Silverio9394 <47988486+Silverio9394@users.noreply.github.com>
2019-03-18 20:37:56 +0300

    feat:update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0; 
+  for (var i = 0, length = preferences.length; i < length ; i++) {
+    let value = preferences[i] - 1;
+    if (value == i) continue;
+    let value2 = preferences[value] - 1;
+    if (value == value2) continue;
+    let value3 = preferences[value2] - 1;
+    if (value3 == value2) continue;
+    if (value3 == i) {
+    count++;
+  }
+ } 
+ return count / 3;
 };
